205d014211a7a1774d188b3d5e11c06d787dfe83
Nawaf Abdullah <nawaf.manage@gmail.com>
2021-07-25 22:42:10 +0300

    update

--- pra.cpp
+++ pra.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 using namespace std;
-
+name
 int main() {    
     int number;
 
-    cout << "Enter an integer: ";
+    cout << "Welcome to the Game.. " <<endl;
     cin >> number;
 
     cout << "You entered " << number;    
     return 0;
 }
+
+void init () {
+    
+    string playerName; 
+    double playerAttackPwr, playerDefensePwr ;
+    cout << " Please Assign the player's info:-" <<endl;
+    cout << "Please enter the Player's name... " <<endl;
+    cin >> playerName; 
+   cout << "Now, please enter the Player's Attack Power... " <<endl;  
+
+}
+
 
 
 class PLAYER {
@@ -22,7 +34,7 @@ class PLAYER {
 
 class GAME {
     public: 
-        void Attack (double attackerPower, double defenderPower) {
+        double Attack (double attackerPower, double defenderPower) {
             double battleOutcome = attackerPower - defenderPower; 
             return  battleOutcome;
         }
